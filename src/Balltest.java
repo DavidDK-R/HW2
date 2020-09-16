37a4cb78e0f5f8a55d95d4625400e78883fddb31
DavidDK-R <70637916+DavidDK-R@users.noreply.github.com>
2020-09-16 09:38:58 +0300

    Add files via upload

---
+++ src/Balltest.java
@@ -0,0 +1,7 @@
+public class Balltest { public static void main(String[] args) {
+    Ball b1 = new Ball(100, 100);
+    System.out.println(b1);
+    b1.move(-20, 75);
+    System.out.println(b1);
+}
+}
